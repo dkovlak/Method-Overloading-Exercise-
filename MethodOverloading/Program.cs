4eb501aa0291b9d9b13cdfd570618c92816c74d1
David Kovlak <kovlakdavid4@gmail.com>
2023-08-31 10:53:26 -0700

    Overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            decimal result = a + b;
+            return result;
+        }
+
+        public static string Add(int a, int b, bool c)
+        {
+            int sum = a + b;
+
+            if (c == true)
+            {
+                string result = (sum == 1 || sum == -1) ? $"{sum} dollar" : $"{sum} dollars";
+                return result;
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(-3, 2, true));
         }
     }
 }
